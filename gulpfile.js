f6b2a6cd958128bb6b74df923ebe1581e006c566
Carlos Henrique <carloshpds@gmail.com>
2016-07-02 19:56:41 -0300

    Defines deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
 
-var options = {
-  modules: {
-    unitTests: {
-      webpackTestConfig: {
-        module: {
-          noParse: [
-            /sinon/
-          ]
-        }
-      }
-    }
-  }
-};
-require('basebuild-component')(options);
+var gutil = require('gulp-util');
+
+var bbComponentOptions = {};
+var bbAngularOptions = {};
+
+if(gutil.env._[0] === 'bundle'){
+  require('basebuild-component')(bbComponentOptions);
+} else {
+  require('basebuild-angular')(bbAngularOptions);
+}
